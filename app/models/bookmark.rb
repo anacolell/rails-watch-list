efb00e4d52b90916fdfdb3c8d9f657e3374bcf47
ana colell <anacolell8@gmail.com>
2021-05-21 17:08:52 +0200

    Added upload picture feature

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,7 +1,6 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
-  has_one_attached :photo
   validates :comment, length: { minimum: 6 }
   validates_uniqueness_of :movie_id, scope: [:list_id]
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,4 @@ class Review < ApplicationRecord
   belongs_to :list
   validates :comment, presence: true
   validates :rating, numericality: { only_integer: true }, inclusion: { in: (1..10) }
-  # validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
 end
